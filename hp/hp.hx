327a60eac82ec06052f64eb5180c590141d22745
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-01-25 14:19:04 +0000

    add a skeleton singleton class for HeapsPunk (HP)

---
+++ hp/hp.hx
@@ -0,0 +1,12 @@
+// helper class similar to FP in FlashPunk and HXP in HaxePunk
+
+class HP {
+	public static var scene; // currentScene?
+	public static var dt; // delta time
+
+	public static function helperFunction() {
+		// do something
+	}
+
+
+}
