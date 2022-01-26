1f0ac5bbaf475b787bf1d604d587b07a357f2a40
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-01-26 12:41:45 +0000

    ahhh shit, should commit this first!

--- Heaps.hx
+++ Heaps.hx
@@ -2,8 +2,6 @@
 // re-usable code for the heaps.io framework
 // but apart from the heapspunk helper classes
 // more for my personal use
-
 class Heaps {
-	//public static var
+	// public static var
 }
-
--- hp/HP.hx
+++ hp/HP.hx
@@ -1,17 +1,15 @@
 // helper class similar to FP in FlashPunk and HXP in HaxePunk
-
 class HP {
 	// hopefully just a reference..?
-	public static var scene: h2d.Scene;  // = App.s2d
+	public static var scene:h2d.Scene; // = App.s2d
 
-	public static var dt(get, default) : Float;
+	public static var dt(get, default):Float;
+
 	static function get_dt() {
-    		return hxd.Timer.dt;
-  	}
+		return hxd.Timer.dt;
+	}
 
-	//static function helperFunction() {
-		// do something
-	//}
-
-
+	// static function helperFunction() {
+	// do something
+	// }
 }
