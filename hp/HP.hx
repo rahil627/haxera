dc36ac42850c3cf26d114754aa0d98ddc72767fd
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-01-28 15:22:41 +0000

    nothing

--- hp/HP.hx
+++ hp/HP.hx
@@ -2,11 +2,11 @@ package ra.hp;
 
 // helper class similar to FP in FlashPunk (and HXP in HaxePunk)
 class HP {
-	// hopefully just a reference..?
+	// TODO: hopefully just a reference, like ActionScript..?
 	public static var scene:h2d.Scene; // = App.s2d
 	public static var debugText:h2d.Text;
 
-
+	// TODO: dt passed into update vs Timer.dt?
 	public static var dt(get, default):Float;
 
 	static function get_dt() {
