e977d618c4bb9cd5ccb83a0fb1f5a01dbb7ed8f0
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-01-31 22:20:33 +0000

    oops

--- hp/BareObject.hx
+++ hp/BareObject.hx
@@ -4,11 +4,17 @@ import h2d.Object;
 // barebones code to use as a base
 class BareObject extends Graphics {
 
-	public override function new(parent:Object, x:Float = 100, y:Float = 100, rotation:Float = 0, color:Int = 0xFFFFFF) {
-		super(parent);
+	public override function new(parent:Object, x:Float = 100, y:Float = 100, r:Float = 0) {
+		super(parent); // for Drawable and Graphics parent is needed, not optional!
+		this.x = x;
+		this.y = y;
+		this.rotation = r;
+		
+		// init
 
 	}
 
+	// note: optional; not an override
 	public function update() {
 
 	}
