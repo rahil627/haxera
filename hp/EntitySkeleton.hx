023fe4e8f4631618b4c1361e3fed77a862636c02
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 05:51:02 +0700

    Update EntitySkeleton.hx

--- hp/EntitySkeleton.hx
+++ hp/EntitySkeleton.hx
@@ -1,5 +1,6 @@
 // just something to ensure that all entities provide ways to do certain minimal things
 // like dispose of itself!
+// the skeletal structure of entities
 // this is optional
 interface EntitySkeleton {
     //public function init():Void; // at the moment, the init code is in the constructor! TODO: init in constructor vs in an init function
