23a8618f5b6add9e03e4d022167b3055a0225669
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 03:33:37 +0700

    Update BaseGlob.hx

--- hp/BaseGlob.hx
+++ hp/BaseGlob.hx
@@ -5,14 +5,13 @@
 // more general useful stuff go to ra.Heaps 'n ra.Haxe 'n ra.hp.HP (HeapsPunk)
 // glawble glawble glawb glob
 class BaseGlob {
-	// TODO: might have a problem disposing these...
-	// nahhh, it's alright... maybe can just provide a function to clear them all
-	// i mean, they're static, can't really delete them... can you?
-	// what's the difference between putting it in Main vs here?
-	//public static var entities:Array<Entity>; // TODO: vs Vector? / pre-allocate memory? lol
+	// might have a problem disposing these...
+	// nahhh, it's alright... just provide a function to clear them all
+	// i mean, they're static, can't really delete them... whether they're here or in Main/BaseApp..
+	//public static var entities:Array<Entity>; // TODO: vs Vector? / just pre-allocate the max expected memory? lol, idk!!
 	//public static var world:Layer;
 	//public static var map1:Map<Int, Entity>; // ?? a map for searching by ID?
-	//public static var canvas:h2d.Graphics;
+	//public static var canvas:h2d.Graphics;  // a top-level thing to draw to
 
 	// constants
 	//public static final laserWidth = 50;
