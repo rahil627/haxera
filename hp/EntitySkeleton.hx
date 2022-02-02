d4b0c0de2dc9a41c4153dc468a3c4bf4fec609b4
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 05:50:04 +0700

    Update and rename Updateable.hx to EntitySkeleton.hx

--- hp/EntitySkeleton.hx
+++ hp/EntitySkeleton.hx
@@ -1,7 +1,7 @@
 // just something to ensure that all entities provide ways to do certain minimal things
 // like dispose of itself!
 // this is optional
-interface EntityPrimer {
+interface EntitySkeleton {
     //public function init():Void; // at the moment, the init code is in the constructor! TODO: init in constructor vs in an init function
     //public function update():Void; // what if it doesn't have an update?
     //public function remove():Void; // what if there's nothing to remove? (except losing it's references)
