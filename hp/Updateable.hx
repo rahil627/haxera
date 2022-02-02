2f839a63ed1288a57bc9a2049f7540a433233026
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 05:48:16 +0700

    Update Updateable.hx

--- hp/Updateable.hx
+++ hp/Updateable.hx
@@ -1,7 +1,7 @@
 // just something to ensure that all entities provide ways to do certain minimal things
 // like dispose of itself!
 // this is optional
-interface Updateable {
+interface EntityPrimer {
     //public function init():Void; // at the moment, the init code is in the constructor! TODO: init in constructor vs in an init function
     //public function update():Void; // what if it doesn't have an update?
     //public function remove():Void; // what if there's nothing to remove? (except losing it's references)
