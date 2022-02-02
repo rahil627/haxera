671bf5bfa5c962576c9457871922dcb534df4569
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 02:08:21 +0700

    Update BaseGlob.hx

--- hp/BaseGlob.hx
+++ hp/BaseGlob.hx
@@ -4,9 +4,11 @@
 // more general useful stuff go to ra.Heaps 'n ra.Haxe 'n ra.hp.HP (HeapsPunk)
 // glawble glawble glawb glob
 class BaseGlob {
+	// hopefully just references..?
+	// create and dispose in the main class
 	//public static var entities:Array<Entity>;
 	//public static var world:Layer;
-	
+	//public static var map1:Map<Int, Entity>; // ?? map for searching?
 	//public static var canvas:h2d.Graphics;
 
 	// constants
