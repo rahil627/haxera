04036a07ad6ad75349c8e0d2898f1e844d1840d0
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-04 00:55:07 +0700

    Update BaseGlobal.hx

--- hp/BaseGlobal.hx
+++ hp/BaseGlobal.hx
@@ -1,11 +1,11 @@
 // just an optional bare-bones global class to copy to begin a new project with
 // rename it to something short, or create a short alias in import.hx
-// store references to commonly-used data structures
+// store references to commonly-used data structures here
 // the scope of this is local to this specific game
 class BaseGlobal {
 	// might have a problem disposing these...
 	// nahhh, it's alright... just provide a function to clear them all
-	// i mean, they're static, can't really delete them... whether they're here or in Main/BaseApp..
+	// i mean, they're static, can't really delete them anyway... whether they're here or in Main/BaseApp..
 	// only keep references to these here
 	//public static var entities:Array<Entity>; // TODO: vs Vector? / just pre-allocate the max expected memory? lol, idk!!
 	//public static var world:Layer;
