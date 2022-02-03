3b22c20e8f123a06491c6527ea889c7521fbb92d
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 17:08:01 +0700

    Update GraphicsEx.hx

--- hp/GraphicsEx.hx
+++ hp/GraphicsEx.hx
@@ -2,13 +2,13 @@
 // if static extensions are no fun, just pass in Graphics into a normal static function
 class GraphicsEx {
 
-	// TODO: maybe not a good idea... the drawing code should be done without shortcuts
-	// it's also just better to get used to using the Flash-like api, it's already simple!
+	// TODO: this is pretty wasteful, the drawing code should be done without shortcuts
+	// it's also just better to get used to using the Flash-like api, it's already stupid simple!!!
 	public function drawLine(g:<Class>Graphics, x:Float, y:Float, x2:Float, y2:Float, ?width:Int, ?color:Int, ?alpha:Float) {
 		if (width || color || alpha) // TODO: i have no clue if this really works..
 			this.lineStyle(width, color, alpha);
 		// if (g.x != x || g.y != y)
-		this.moveTo(x, y);
+			this.moveTo(x, y);
 		this.lineTo(x2, y2);
 	       
 	}
