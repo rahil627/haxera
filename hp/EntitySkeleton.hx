74f49c1c4175e9c8d2e2625084051dcdf6597335
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-04 02:59:24 +0700

    Update EntitySkeleton.hx

--- hp/EntitySkeleton.hx
+++ hp/EntitySkeleton.hx
@@ -4,6 +4,8 @@
 // this is completely optional
 // example use: BaseEntity implements EntitySkeleton
 //interface EntitySkeleton {
+    //public var(default, never) id;
+
     //public function init():Void; // at the moment, the init code is in the constructor! TODO: init in constructor vs in an init function
     //public function update():Void; // what if it doesn't have an update?
     //public function remove():Void; // what if there's nothing to remove? (except losing it's references)
