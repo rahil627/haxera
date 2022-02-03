1acf6172f7f8ae86098e1fccf8311f835eeb21e7
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 19:26:42 +0700

    Create import.hx

---
+++ hp/import.hx
@@ -0,0 +1,8 @@
+import ra.hp.HP
+
+// aliases
+// holy shit(!!) this is awesome!!
+//import dn.Color as C; // one letter might be a problem refectoring..? regex 'C\.'?
+//import tools.LPoint as P;
+//import GameGlobal as GG; // TODO: refactor
+//import HeapsPunkGlobal as HP // or P? or Punk? // TODO: refactor
