681a021087f5b9a86fe6a9b02cfdd4a853cda7b9
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 19:35:49 +0700

    Update import.hx

--- hp/import.hx
+++ hp/import.hx
@@ -1,9 +1,7 @@
 // add these to a "import.hx" file in the root directory of the heaps app
-import ra.hp.HP
 
 // aliases
 // holy shit(!!) this is awesome!!
 //import dn.Color as C; // one letter might be a problem refectoring..? regex 'C\.'?
 //import tools.LPoint as P;
-//import Global as GG; // GameGlobal TODO: refactor
-//import Global as HP // HeapsPunkGlobal, or P? or Punk? // TODO: refactor
+//import ra.hp.Global as HP // HeapsPunkGlobal, or P? or Punk? // TODO: refactor
