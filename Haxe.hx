e6a27d3e6925bc5b66264aa1cd0eb0fa050d3f15
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-04 02:45:48 +0700

    Update Haxe.hx

--- Haxe.hx
+++ Haxe.hx
@@ -7,7 +7,7 @@ package ra; // is this not implied by the path name?
 // more for my own personal use / not useful to others
 class Haxe {
 
-	// hmm, here vs MathEx
+	// hmm, here vs MathEx (currently in both...)
 	public static inline function randomHex():Int {
 		return Haxe.randomInt(0xFFFFFF);
 	}
