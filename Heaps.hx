42317b8fec4ab69c5fac5518393f4021115596ef
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 22:22:55 +0700

    Update Heaps.hx

--- Heaps.hx
+++ Heaps.hx
@@ -3,7 +3,7 @@ package ra;
 // HeapsHelper.hx?
 // re-usable code for the heaps.io framework
 // but apart from the heapspunk helper classes
-// more for my personal use
+// more for my own personal use
 class Heaps {
 	// public static var
 
