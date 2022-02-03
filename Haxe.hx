6e1338a44283c966c9b8907bc61d7d5057ab900b
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 22:41:58 +0700

    Update Haxe.hx

--- Haxe.hx
+++ Haxe.hx
@@ -6,12 +6,12 @@ package ra; // is this not implied by the path name?
 // including the use of the standard library that comes with it
 // there should be no other dependencies
 class Haxe {
-	public static inline function randomInt():Int {
-		return Math.round(Math.random());
+	public static inline function randomInt(max = Int64.high):Int { // TODO: 32bit max?
+		return std.int(Math.random(max));
 	}
 	
 	public static inline function randomHex():Int {
-		return Haxe.randomInt() * 0xFFFFFF; //
+		return Haxe.randomInt(0xFFFFFF);
 	}
 	
 	
