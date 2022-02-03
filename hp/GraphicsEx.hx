2ea0f26b14a0bde23d5fc2ac53705be35923e45c
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 17:07:10 +0700

    Update GraphicsEx.hx

--- hp/GraphicsEx.hx
+++ hp/GraphicsEx.hx
@@ -4,9 +4,10 @@ class GraphicsEx {
 
 	// TODO: maybe not a good idea... the drawing code should be done without shortcuts
 	// it's also just better to get used to using the Flash-like api, it's already simple!
-	public function drawLine(cl:<Class>Graphics, x:Float, y:Float, x2:Float, y2:Float, ?width:Int, ?color:Int, ?alpha:Float) {
+	public function drawLine(g:<Class>Graphics, x:Float, y:Float, x2:Float, y2:Float, ?width:Int, ?color:Int, ?alpha:Float) {
 		if (width || color || alpha) // TODO: i have no clue if this really works..
-			this.lineStyle(width, color, alpha); 
+			this.lineStyle(width, color, alpha);
+		// if (g.x != x || g.y != y)
 		this.moveTo(x, y);
 		this.lineTo(x2, y2);
 	       
