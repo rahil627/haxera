0ddd4af103b6aab448640b21ca7b9ca276b1802a
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-03 19:34:41 +0700

    Update import.hx

--- hp/import.hx
+++ hp/import.hx
@@ -5,5 +5,5 @@ import ra.hp.HP
 // holy shit(!!) this is awesome!!
 //import dn.Color as C; // one letter might be a problem refectoring..? regex 'C\.'?
 //import tools.LPoint as P;
-//import GameGlobal as GG; // TODO: refactor
-//import HeapsPunkGlobal as HP // or P? or Punk? // TODO: refactor
+//import Global as GG; // GameGlobal TODO: refactor
+//import Global as HP // HeapsPunkGlobal, or P? or Punk? // TODO: refactor
