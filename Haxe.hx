137c931a32c08a86e4307cbd25294c99f76b67ad
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-04 02:44:31 +0700

    Update Haxe.hx

--- Haxe.hx
+++ Haxe.hx
@@ -2,9 +2,9 @@ package ra; // is this not implied by the path name?
 
 // HaxeHelper.hx? HH import alias
 // i mean... there's only a haxe package, not a class, soooo...
-// re-usable code for the haxe programming language
-// including the use of the standard library that comes with it
+// re-usable code for the haxe programming language (including std lib)
 // there should be no other dependencies
+// more for my own personal use / not useful to others
 class Haxe {
 
 	// hmm, here vs MathEx
