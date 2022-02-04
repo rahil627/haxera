7574e21a911ce660032b11f8bd8f9bf086640a4d
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-05 02:33:20 +0700

    Update PunkApp.hx

--- hp/PunkApp.hx
+++ hp/PunkApp.hx
@@ -32,8 +32,8 @@ class PunkApp extends App {
 
 		// set global vars
 		HP.app = this;
-		//HP.scene = this.s2d; // decided to just get everything in the class properties
-		//HP.window = hxd.Window.getInstance();
+		HP.scene = this.s2d; // just feels better setting it here, rather than putting it in a get-property
+		HP.window = hxd.Window.getInstance();
 	
 		// init global stuff
 		HP.window.title = "heaps <3 p u n k"; // i've seen this set in the hxml build file
