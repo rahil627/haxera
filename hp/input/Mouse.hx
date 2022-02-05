2814966933f0976191b0d978c3257be63ce538a7
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 02:39:41 +0700

    Update Mouse.hx

--- hp/input/Mouse.hx
+++ hp/input/Mouse.hx
@@ -12,11 +12,11 @@ class Mouse { // or/and Touch? TODO: is this the same for touch input?
 	public static var leftJustReleased(get, null):Bool;
 	
 	// properties
-	public static function get_x() return HP.scene.mouseX;
+	public static function get_x() return HP.scene.mouseX; // strange location!
 	public static function get_y() return HP.scene.mouseY;
 	public static function get_leftIsDown():Bool return Key.isDown(Key.MOUSE_LEFT);
-	public static function leftJustClicked():Bool return Key.isPressed(Key.MOUSE_LEFT); // isPressed is confusing :(
-	public static function leftJustReleased():Bool return Key.isReleased(Key.MOUSE_LEFT);
+	public static function get_leftJustClicked():Bool return Key.isPressed(Key.MOUSE_LEFT); // isPressed is confusing :(
+	public static function get_leftJustReleased():Bool return Key.isReleased(Key.MOUSE_LEFT);
 	
 	
 }
