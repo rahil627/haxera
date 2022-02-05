de1626a27737b900df6382e48d62b729acd5c001
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 02:09:52 +0700

    Create KeyEx.hx

---
+++ hp/input/KeyEx.hx
@@ -0,0 +1,22 @@
+package ra.hp.input;
+
+import hxd.Key;
+
+// just a stupid extension to avoid being confused by isPressed
+class KeyEx {
+
+  // TODO: no clue how inline actually works
+    public static inline function isDown( code : Int ) {
+        return Key.keyPressed[code] > 0;
+    }
+	
+    public static inline function justPressed( code : Int ) {
+	    return Key.keyPressed[code] == getFrame() - 1;
+	}
+
+	public static inline justReleased( code : Int ) {
+		return Key.keyPressed[code] == -getFrame() + 1;
+    }
+  
+  
+}
