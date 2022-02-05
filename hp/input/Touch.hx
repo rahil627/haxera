7821002d6264a4802767f22b57788069597f2fbb
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 04:38:49 +0700

    Update Touch.hx

--- hp/input/Touch.hx
+++ hp/input/Touch.hx
@@ -3,11 +3,14 @@ package ra.hp.input;
 import ra.hp.HP; // depends on HP!!
 import hxd.Key;
 
-// it feels like touch input was hacked in by heaps, inferior to mouse
-// this ds brings makes a home for it
+// touch input in heaps feel as if it was done secondary to mouse
+// this ds brings makes a home for it :)
 class Touch {
 	// currently just copy-pasta from Mouse, because mouseX, mouseY and Key.MOUSE_LEFT are also used for touch input, but
 	// beyond that, however, they are very different: multi-touch, no other mouse buttons nor wheel
+	
+	// TODO: multi-touch
+	// TODO: 3 levels: global, scene, interactive
 	
 	// TODO: inline?	
 	public static var x(get, null):Float; 
@@ -18,7 +21,7 @@ class Touch {
 	// note: stuff like onOver/onOut are handled by Interactive, not here
 	
 	// properties
-	public static function get_x() return HP.scene.mouseX; // strange location!
+	public static function get_x() return HP.scene.mouseX; // note: scene-based event, not global
 	public static function get_y() return HP.scene.mouseY;
 	public static function get_isTouching():Bool return Key.isDown(Key.MOUSE_LEFT);
 	public static function get_justTouched():Bool return Key.isPressed(Key.MOUSE_LEFT); // isPressed is confusing :(
