30181171e64dce42e42d673c2a9bccbd59e01257
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 02:52:03 +0700

    Update KeyEx.hx

--- hp/input/KeyEx.hx
+++ hp/input/KeyEx.hx
@@ -3,12 +3,14 @@ package ra.hp; // note: not ra.hp.input
 import hxd.Key;
 
 // just a stupid extension to avoid being confused by isPressed
+// onDown/onUp = justPressed/justReleased; choose what you prefer!
 class KeyEx {
 
   // TODO: no clue how inline actually works
     public static inline function isDown(cl:Class<Key>, code:Int ) return Key.keyPressed[code] > 0; // duplicate error?
+    public static inline function onDown(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == Key.getFrame() - 1; 
+    public static inline function onUp(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == -Key.getFrame() + 1;
     public static inline function justPressed(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == Key.getFrame() - 1;
-    public static inline function justReleased(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == -Key.getFrame() + 1;
-  
+    public static inline function justReleased(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == -Key.getFrame() + 1;  
   
 }
