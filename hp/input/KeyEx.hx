dd6c0b54d71fe32319591a1b7e1385c5eeaf4dec
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 02:11:45 +0700

    Update KeyEx.hx

--- hp/input/KeyEx.hx
+++ hp/input/KeyEx.hx
@@ -8,7 +8,7 @@ class KeyEx {
   // TODO: no clue how inline actually works
     public static inline function isDown( code : Int ) return Key.keyPressed[code] > 0;
     public static inline function justPressed( code : Int ) return Key.keyPressed[code] == getFrame() - 1;
-    public static inline justReleased( code : Int ) return Key.keyPressed[code] == -getFrame() + 1;
+    public static inline function justReleased( code : Int ) return Key.keyPressed[code] == -getFrame() + 1;
   
   
 }
