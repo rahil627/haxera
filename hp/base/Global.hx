2d204494ca2c44dea22b1c00475ed424a005ad79
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-05 23:16:26 +0700

    Update Global.hx

--- hp/base/Global.hx
+++ hp/base/Global.hx
@@ -6,25 +6,23 @@ package ra.hp.base
 // the scope of this is local to this specific game
 // TODO: singleton vs static
 class Global {
-	// might have a problem disposing these...
-	// nahhh, it's alright... just provide a function to clear them all
-	// i mean, they're static, can't really delete them anyway... whether they're here or in Main/BaseApp..
 	// only keep references to these here
 	
 	// i think this is the only important data structure to choose
-	// vector seems like a pain to maintain--you must know the index to use v.set()--, soooo... array vs list vs map
-	// the simplest and laziest way is to just use list and forget about it :)
-	// given that you are constantly adding and removing stuff all the time, maybe it's the best too?
-	// then comes arrays vs map
+	// give that you're constantly adding ' removing stuff, such as projectiles, then
+	// List is not only the best option, but the simplest too!
+	// array / vector and map are generally for optimization purposes only
 	// array is O(1) access complexity vs O(log(n)) access complexity
-	// ..but really tho, it'd be limited to things you're constantly accessing, and if you're constantly accessing them..,
-	// well, you'd probably be holding pointers/references to those things anyway (i.e. players)!, soooo...:
-	//public static var entities:List<Entity>;
+	// ..but really tho, it'd be limited to things you're constantly accessing,
+	// and if you're already constantly accessing them.., well,
+	// you'd probably already have stored pointers/references to those things anyway (i.e. players)!, soooo...:
+	//public static var entities:List<BaseEntity>;
 	
-	// maybe later on, can use stuff like this:
-	//public static var players:Vector(PlayerEntity) // just an example
-	//public static var mcaEntities:Map<Int, Entity>; // an optional map to search stuff by id
-	
+	// in special cases can use stuff like this:
+	//public static var players:Vector<Player>; // if the max amount of players never change, otherwise:
+	//public static var players:Array<Player>
+	//public static var entitiesBackpack:Map<Int, TargetEntities>; // to search for commonly-searched stuff by id (key)	
+
 	//public static var world:Layer;
 	//public static var canvas:h2d.Graphics;  // a top-level thing to draw to
 
