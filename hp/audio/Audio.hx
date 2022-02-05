b1d08a7a48253a6df4bd83791ac02c771411d4f3
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 05:52:53 +0700

    Update Audio.hx

--- hp/audio/Audio.hx
+++ hp/audio/Audio.hx
@@ -1,3 +1,34 @@
+package ra.hp.audio;
+//package terryheaps;
+
+//Very lightweight for Ludum Dare
+class SimpleAudio {
+	public static function init(){
+		loadedsounds = new Map<String, hxd.res.Sound>();
+	}
+
+	public static function load(soundFileName:String){
+		if(!loadedsounds.exists(soundFileName)){
+			loadedsounds.set(soundFileName, hxd.Res.load("filepath/" + soundFileName + "extension").toSound());
+		}
+	}
+
+	public static function play(soundfile:String){
+		var soundresource:hxd.res.Sound;
+
+		if(loadedsounds.exists(soundfile)){
+			soundresource = loadedsounds.get(soundfile);
+		}else{
+			soundresource = hxd.Res.load("sounds/" + soundfile + ".mp3").toSound();
+			loadedsounds.set(soundfile, soundresource);
+		}
+
+		soundresource.play();
+	}
+
+	public static var loadedsounds:Map<String, hxd.res.Sound>;
+}
+
 
 
 /* from ld40
