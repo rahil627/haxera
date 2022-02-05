05aafcab90031c9c8c2db06db7172b85ff4d33ba
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 00:36:09 +0700

    Update and rename hp/base/BaseEntity.hx to hp/basEntity.hx

--- hp/basEntity.hx
+++ hp/basEntity.hx
@@ -14,7 +14,7 @@ enum EntityKind {
 // i'm not really sure what entity means,
 // but this one will likely contain an Object, or something that extends an Object,
 // which itself will be added to the Scene (or first to Layers, then Scene)
-class BaseEntity /*implements BaseEntitySkeleton*/ {	
+class Entity /*implements EntitySkeleton*/ {	
 	//var id:Int; // maybe useful for finding it later..?, can be used as a map key, TODO: storing temp int vs reference?
 	//var name:String; // although strange, i've seen HaxePunk and Armory use this for searching, secret optimization magic??
 	//var kind:EntityKind; // maybe useful narrowing down searches; one big container vs keeping multiple containers
@@ -28,8 +28,8 @@ class BaseEntity /*implements BaseEntitySkeleton*/ {
 		//this.x = x; // just a wrapper for e.sprite.x?
 		//this.y = y;
 		//sprite = new h2d.Anim(getAnim(), 15);
-		//BaseGlobal.world.add(sprite, hasFlag(Under) ? LayerKind.under : LayerKind.players);
-		//BaseGlobal.entities.push(this); // 1 complexity
+		//Global.world.add(sprite, hasFlag(Under) ? LayerKind.under : LayerKind.players);
+		//Global.entities.push(this); // 1 complexity
 	}
 
 	// override me! and remember to call super.update(dt)
@@ -43,7 +43,7 @@ class BaseEntity /*implements BaseEntitySkeleton*/ {
 	// override me! and remember to call super.remove()
 	public function remove() {
 		//sprite.remove(); // all Objects provide this
-		//BaseGlobal.entities.remove(this); // n complexity
+		//Global.entities.remove(this); // n complexity
 		//this = null; // TODO: lol, not sure about doing this from within the class...
 	}
 
@@ -52,12 +52,12 @@ class BaseEntity /*implements BaseEntitySkeleton*/ {
 
 	//Tells if a value v is of the type t. Returns false if v or t are null.
 	//If t is a class or interface with @:generic meta, the result is false.
-	public function is<T:BaseEntity>(c:Class<T>) return Std.isOfType(this, c); // like entity kind, useful to narrow down searches
+	public function is<T:Entity>(c:Class<T>) return Std.isOfType(this, c); // like entity kind, useful to narrow down searches
 	
 	//Checks if object value is an instance of class or interface c.
 	//Compiles only if the type specified by c can be assigned to the type of value.
 	//This method checks if a downcast is possible. That is, if the runtime type of value is assignable to the type specified by c, value is returned. Otherwise null is returned.
 	//This method is not guaranteed to work with core types such as String, Array and Date.
 	//If value is null, the result is null. If c is null, the result is unspecified.
-	public function as<T:BaseEntity>(c:Class<T>) : T return Std.downcast(this, c); // ?? not sure when to use, but seems handy!
+	public function as<T:Entity>(c:Class<T>) : T return Std.downcast(this, c); // ?? not sure when to use, but seems handy!
 }
