112e5360789bec27032106547bb6155295b4f3aa
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 01:17:34 +0700

    Update Mouse.hx

--- hp/input/Mouse.hx
+++ hp/input/Mouse.hx
@@ -3,41 +3,20 @@ package ra.hp; // note: not ra.hp.input
 import package ra.hp.HP; // depends on HP!!
 import hxd.Key;
 
-// a stupid class for stupid people like me
-class Mouse {
+// a stupid class for stupid people like me :(
+class Mouse { // or/and Touch? TODO: is this the same for touch input?
 	public static var x(get, null):Float; 
-	public static var y(get, null):Float;	
-	
-	
-	
-	
-	
-	
-	
-	
+	public static var y(get, null):Float;
+	public static var leftIsDown(get, null):Bool;
+	public static var leftClicked(get, null):Bool;
+	public static var leftReleased(get, null):Bool;
 	
 	// properties
 	public static function get_x() return HP.scene.mouseX;
 	public static function get_y() return HP.scene.mouseY;
-
-	public static function leftheld():Bool {
-		if (Key.isDown(Key.MOUSE_LEFT)) {
-			return true;
-		}
-		return false;
-	}
-
-	public static function leftclick():Bool {
-		if (Key.isPressed(Key.MOUSE_LEFT)) {
-			return true;
-		}
-		return false;
-	}
-
-	public static function leftreleased():Bool {
-		if (Key.isReleased(Key.MOUSE_LEFT)) {
-			return true;
-		}
-		return false;
-	}
+	public static function get_leftIsDown():Bool return Key.isDown(Key.MOUSE_LEFT);
+	public static function get_leftClicked():Bool return Key.isPressed(Key.MOUSE_LEFT); // isPressed is confusing :(
+	public static function get_leftReleased():Bool return Key.isReleased(Key.MOUSE_LEFT);
+	
+	
 }
