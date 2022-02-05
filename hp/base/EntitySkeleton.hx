8ea7081e789431e3c578bba4b866ad318a229f81
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-05 23:32:41 +0700

    Update EntitySkeleton.hx

--- hp/base/EntitySkeleton.hx
+++ hp/base/EntitySkeleton.hx
@@ -1,10 +1,12 @@
+package ra.hp.base // delete this line!
+
 // just something to ensure(/insure?) that all entities provide ways to do certain minimal things
 // like dispose of itself!
 // the skeletal structure of entities
 // this is completely optional
 // example use: BaseEntity implements EntitySkeleton
-//interface EntitySkeleton {
-    //public function init():Void; // at the moment, the init code is in the constructor! TODO: init in constructor vs in an init function
-    //public function update(dt:Float):Void; // what if it doesn't have an update?
-    //public function remove():Void; // what if there's nothing to remove? (except losing it's references)
-//}
+interface EntitySkeleton {
+    public function init():Void; // at the moment, the init code is in the constructor! TODO: init in constructor vs in an init function
+    public function update(dt:Float):Void; // what if it doesn't have an update?
+    public function remove():Void; // what if there's nothing to remove? (except losing it's references)
+}
