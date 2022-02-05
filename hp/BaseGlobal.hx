28927a3921ab6ba2e7ce0d5dec7b854b917a2a52
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-05 20:28:35 +0700

    Update BaseGlobal.hx

--- hp/BaseGlobal.hx
+++ hp/BaseGlobal.hx
@@ -8,7 +8,7 @@ class BaseGlobals {
 	// nahhh, it's alright... just provide a function to clear them all
 	// i mean, they're static, can't really delete them anyway... whether they're here or in Main/BaseApp..
 	// only keep references to these here
-	//public static var entities:Array<Entity>; // TODO: vs Vector? / just pre-allocate the max expected memory? lol, idk!!
+	//public static var entities:Array<Entity>; // vector seems like a pain to maintain
 	//public static var world:Layer;
 	//public static var map1:Map<Int, Entity>; // ?? a map for searching by ID? log(n) complexity for access?
 	//public static var canvas:h2d.Graphics;  // a top-level thing to draw to
