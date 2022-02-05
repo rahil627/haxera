3206006b52fc827c929404c91f3eb8cc5e7717df
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 05:39:37 +0700

    Create Audio.hx

---
+++ hp/audio/Audio.hx
@@ -0,0 +1,10 @@
+
+
+/* from ld40
+		while( true )
+			try winds.push(hxd.Res.load("sfx/wind" + (i++) + ".wav").toSound()) catch( e : hxd.res.NotFound ) break;
+
+		// preload sounds
+		for( s in hxd.Res.load("sfx") )
+			s.toSound().getData();
+*/
