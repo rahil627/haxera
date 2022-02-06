0f2a874c5c8da67c8a7d0ad01fbce36ddacff788
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-07 02:01:55 +0700

    Update KeyEx.hx

--- hp/input/KeyEx.hx
+++ hp/input/KeyEx.hx
@@ -7,7 +7,7 @@ import hxd.Key;
 class KeyEx {
 
   // TODO: no clue how inline actually works
-    public static inline function isDown(cl:Class<Key>, code:Int ) return Key.keyPressed[code] > 0; // duplicate error?
+    public static inline function isDown(cl:Class<Key>, code:Int ) return Key.keyPressed[code] > 0; // TODO: maybe duplicate error?
     public static inline function onDown(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == Key.getFrame() - 1; 
     public static inline function onUp(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == -Key.getFrame() + 1;
     public static inline function justPressed(cl:Class<Key>, code:Int ) return Key.keyPressed[code] == Key.getFrame() - 1;
