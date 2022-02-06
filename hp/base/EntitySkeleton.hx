fa70742f80a1d93120ac8482e4ca8dd6ab70a4ed
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-07 06:02:05 +0700

    Update EntitySkeleton.hx

--- hp/base/EntitySkeleton.hx
+++ hp/base/EntitySkeleton.hx
@@ -6,6 +6,6 @@ package hp.base // DELETE ME!!
 // example use: BaseEntity implements EntitySkeleton
 interface EntitySkeleton {
     //public function init():Void; // at the moment, the init code is in the constructor! i like it better there...
-    public function update(dt:Float):Void; // called by Entities.update(), used in the main update loop
-    public function remove():Void; // called by Entities.remove(), used in the main remove everything function
+    public function update(dt:Float):Void; // called in EntityList's update(), used in the main update loop
+    public function remove():Void; // called in EntityList's remove(), used in the main remove everything function
 }
