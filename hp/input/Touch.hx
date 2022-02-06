a31cd4ac3f38e2735745c7800e984687f9f5ab25
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-07 01:39:20 +0700

    Update Touch.hx

--- hp/input/Touch.hx
+++ hp/input/Touch.hx
@@ -18,7 +18,7 @@ class Touch {
 	public static var x(get, null):Float; 
 	public static var y(get, null):Float;
 	public static var isTouching(get, null):Bool;
-	public static var justTouched(get, null):Bool; // "just" might be too verbose :/
+	public static var justBeganTouching(get, null):Bool; // "just" might be too verbose :/
 	public static var justStoppedTouching(get, null):Bool; // just let go'd?
 	// note: stuff like onOver/onOut are handled by Interactive, not here
 	
@@ -26,7 +26,7 @@ class Touch {
 	public static function get_x() return HP.scene.mouseX; // note: scene-based event, not global
 	public static function get_y() return HP.scene.mouseY;
 	public static function get_isTouching():Bool return Key.isDown(Key.MOUSE_LEFT);
-	public static function get_justTouched():Bool return Key.isPressed(Key.MOUSE_LEFT); // isPressed is confusing :(
+	public static function get_justBeganTouching():Bool return Key.isPressed(Key.MOUSE_LEFT); // isPressed is confusing :(
 	public static function get_justStoppedTouching():Bool return Key.isReleased(Key.MOUSE_LEFT);
 	
 	
