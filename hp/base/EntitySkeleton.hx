78e864149c9b49a002e14d78f9a1f49f52207e9e
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 07:46:09 +0700

    Update EntitySkeleton.hx

--- hp/base/EntitySkeleton.hx
+++ hp/base/EntitySkeleton.hx
@@ -7,6 +7,6 @@ package ra.hp.base // DELETE ME!!
 // example use: BaseEntity implements EntitySkeleton
 interface EntitySkeleton {
     //public function init():Void; // at the moment, the init code is in the constructor!
-    public function update(dt:Float):Void; // what if it doesn't have an update?
-    public function remove():Void; // what if there's nothing to remove? (except losing it's references)
+    //public function update(dt:Float):Void; // what if it doesn't have an update?
+    public function remove():Void; // what if there's nothing to remove(?), except itself
 }
