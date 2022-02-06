33de957102031269a16e09dbb3822a335e45a1e7
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 18:09:35 +0700

    Update Haxe.hx

--- Haxe.hx
+++ Haxe.hx
@@ -2,11 +2,10 @@ package ra; // is this not implied by the path name?
 
 import haxe.Math;
 
-// HaxeHelper.hx? HH import alias
-// re-usable code for the haxe programming language (including std lib)
+// a helper/utility ds for the haxe programming language (including std lib)
 // more for my own personal use / not useful to others
-// note to self: there should be no other dependencies
-class Haxe {
+// note to self: there should no dependencies beyond the haxe package
+class Haxe { // todo: HaxeHelper.hx? HH import alias
 
 	public static inline function randomInt(max = Int64.high):Int { // TODO: 32bit max?
 		return std.int(Math.random(max));
