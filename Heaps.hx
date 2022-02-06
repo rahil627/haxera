87c2f7bad644f48759b3b0e69625dd849eea2569
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 18:10:32 +0700

    Update Heaps.hx

--- Heaps.hx
+++ Heaps.hx
@@ -3,6 +3,7 @@ package ra;
 // a helper/utility ds for the heaps.io framework
 // but apart from the HeapsPunk helper ds'es
 // more for my own personal use / not useful to others
+// note to self: there should no dependencies beyond the heaps 'n haxe packages
 class Heaps { // todo: HeapsHelper.hx?
 	// public static var
 
