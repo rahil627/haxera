8ad696e12fd67784553e776b2b09e24177609275
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-07 01:05:18 +0700

    Update GraphicsEx.hx

--- hp/GraphicsEx.hx
+++ hp/GraphicsEx.hx
@@ -1,5 +1,4 @@
 package hp; // TODO: i'm not sure if this needs to be in HeapsPunk or not...
-//package hxd.GraphicsEx // ? lol
 
 // note: don't use 'import', use 'using': using ra.hp.GraphicsEx
 // if static extensions are no fun, just pass in Graphics into a normal static function
