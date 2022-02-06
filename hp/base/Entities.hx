e4425f989d7e8aeb3f12faff5c52b63c760fbf09
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 19:51:59 +0700

    Update Entities.hx

--- hp/base/Entities.hx
+++ hp/base/Entities.hx
@@ -1,24 +1,45 @@
-package ra.hp.entities;
-//package elke.entity;
+package ra.hp.base.entities; // DELETE ME
+
+import ra.hp.base.Entity // DELETE ME
 
 // a specialized container for Entity
 // designed to be an extendable replacement for haxe.ds.List or Array
 class Entities {
+	
 	//@:allow(elke.entity.Entity3D, elke.entity.Entity2D)
 	//static var _NEXT_ID = 0;
+	
 	var entities:List<Entity>; // or Array
 	
 	public function new() {
 		entities = [];
 	}
 
-	function add(e:Entity) {
-		entities.add(e); // adds to the end of the list; use push to add to to the beginning
+	// adds to the beginning of the list
+	function add(e:Entity) { // "add" is a personal preference
+		entities.push(e);
 	}
 	
-	function pop(e:Entity) {
-		entities.pop(); // removes first element
+	function push(e:Entity) {
+		entities.push(e);	
 	}
+	
+	// removes one element from the beginning of the list
+	function remove() {
+		entities.pop();
+	}
+
+	// note: use this to try to keep more permanent stuff at the end
+	function addToEnd(e:Entity) {
+		entities.add(e);
+	}
+	
+	//function removeFromEnd(e:Entity) {
+		// not sure how to remove the last element...
+		// maybe it's a single-linked-list impl?, not double?
+		// strangely last() is provided, which returns the last element,
+		// but then you'd have to still use remove() and go through the entire list to remove it
+	//}
 
 	function remove(e:Entity) {
 		entities.remove(e);
