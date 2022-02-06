7c54f89b0e65ac45244b84e5113cc86e84eae806
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 17:54:57 +0700

    Update Haxe.hx

--- Haxe.hx
+++ Haxe.hx
@@ -1,14 +1,15 @@
 package ra; // is this not implied by the path name?
 
+import haxe.Math;
+
 // HaxeHelper.hx? HH import alias
 // re-usable code for the haxe programming language (including std lib)
 // more for my own personal use / not useful to others
 // note to self: there should be no other dependencies
 class Haxe {
 
-	// hmm, here vs MathEx (currently in both...)
 	public static inline function randomHex():Int {
-		return Haxe.randomInt(0xFFFFFF);
+		return Math.randomInt(0xFFFFFF);
 	}
 	
 	
