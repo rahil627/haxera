c7730461a7a0be5686ec78f7f087c0ab77394bd9
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 21:43:53 +0700

    Update Entities.hx

--- hp/base/Entities.hx
+++ hp/base/Entities.hx
@@ -3,9 +3,9 @@ import ra.hp.base.Entity; // DELETE ME
 
 import haxe.ds.list;
 
-// a specialized container for Entity
+// a specialized generic container for Entity
 // designed to be an extendable replacement for haxe.ds.List or Array
-class Entities {
+class Entities<T:Entity> { // in HaxePunk, it extends Entity??
 	
 	// maybe can use inheritance (extends List) or create a new generic (entities:ListEx<Entity>) somehow..?
 	// see https://haxe.org/manual/type-system-generic.html
@@ -16,22 +16,24 @@ class Entities {
 	//@:allow(elke.entity.Entity3D, elke.entity.Entity2D)
 	//static var _NEXT_ID = 0;
 	
-	public var entities:List<Entity>;
-	//public var entities:Array<Entity>; // choose your ds impl
+	public var entities:List<Entity:T>;
+	//public var entities:Array<Entity:T>; // choose your ds impl
 	
 	// the core of haxe.ds.List and Array
 	
+	public var count(get, never):Int; function get_count() return entities.length;
+	
 	public function new() {
-		entities = [];
+		entities = new List();
 	}
 
 	// adds an entity to the beginning of the list
-	function add(e:Entity) { // i personally prefer "add" to keep it simple
-		entities.push(e);
+	function add(entity:T) { // i personally prefer "add" to keep it simple
+		entities.push(entity);
 	}
 	
-	function push(e:Entity) {
-		entities.push(e);	
+	function push(entity:T) {
+		entities.push(entity);	
 	}
 	
 	// removes one entity from the beginning of the list
@@ -40,11 +42,11 @@ class Entities {
 	}
 
 	// note: use this to try to keep more permanent stuff at the end
-	function addToEnd(e:Entity) {
-		entities.add(e);
+	function addToEnd(entity:T) {
+		entities.add(entity);
 	}
 	
-	//function removeFromEnd(e:Entity) {
+	//function removeFromEnd(entity:T) {
 		// not sure how to remove the last element...
 		// maybe it's a single-linked-list impl?, not double?
 		// strangely last() is provided, which returns the last element,
@@ -52,8 +54,8 @@ class Entities {
 	//}
 
 	// n complexity, but surely has optimization magic in it
-	function remove(e:Entity) {
-		entities.remove(e);
+	function remove(entity:T) {
+		entities.remove(entity);
 	}
 	
 	
@@ -65,17 +67,17 @@ class Entities {
 	
 	public function dispose() {
 		for (e in container)
-			e.remove();
+			entity.remove();
 		entities.clear();
 	}
 
 	public function update(dt:Float) {
 		for (e in container)
-			e.update(dt);
+			entity.update(dt);
 	}
 	
 	/*
-	public function search(e:Entity)Entity {
+	public function search(entity:T)T {
 		for (i in entities) {
 			//if (i.kind != e.kind)
 				//return null; // todo: or false?
@@ -86,7 +88,7 @@ class Entities {
 			
 	}
 	
-	public function reverseSearch(e:Entity)Entity {
+	public function reverseSearch(entity:T)T {
 		for (var i = i.entities.length(), i >=0, i--) {
 			// do stuff
 		}
