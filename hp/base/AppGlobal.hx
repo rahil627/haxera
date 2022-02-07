3a65bc1a8ce1fb3602d53952ba605816c1d174cc
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-08 00:12:49 +0700

    Update AppGlobal.hx

--- hp/base/AppGlobal.hx
+++ hp/base/AppGlobal.hx
@@ -1,12 +1,14 @@
+
+// just an optional bare-bones global class to copy to begin a new project with
+// rename it to something short, or create a short alias such as 'GG' in import.hx
 package hp.base // DELETE ME!!
 
 import haxe.ds.List;
 import h2d.Layers;
-	
-// just an optional bare-bones global class to copy to begin a new project with
-// rename it to something short, or create a short alias such as 'GG' in import.hx
+
+// a static global helper class, with a scope specific to this app
 // store references to commonly-used data structures here
-// the scope of this is local to this specific app
+// try to store only references of stuff, no actual objects (except simple types)
 // TODO: singleton vs static
 class AppGlobal {
 	
