bd1f06830e104f3492a546a0e09e5fd4bb221c8a
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-07 20:53:26 +0700

    Update EntitySkeleton.hx

--- hp/base/EntitySkeleton.hx
+++ hp/base/EntitySkeleton.hx
@@ -6,7 +6,7 @@ package hp.base // DELETE ME!!
 // those functions can be empty, so no worries
 // example use: BaseEntity implements EntitySkeleton
 interface EntitySkeleton {
-    //public function init():Void; // at the moment, the init code is in the constructor! i like it better there...
+    //public function init():Void; // in case you need to separate constructor from init
     //public function add():Void; // in case you need to init something, but not add it to the scene yet
     public function update(dt:Float):Void; // called in EntityList's update(), used in the main update loop
     public function remove():Void; // called in EntityList's remove(), used in the main remove everything function
