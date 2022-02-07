880ce63925fde37e086e83e910274b69fceec8ee
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-08 00:23:51 +0700

    Update EntitySkeleton.hx

--- hp/base/EntitySkeleton.hx
+++ hp/base/EntitySkeleton.hx
@@ -1,3 +1,5 @@
+// just an optional bare-bones ds to copy and begin a new project with
+// delete these lines and you're good to go!
 package hp.base; // DELETE ME!!
 import hp.base.*; // DELETE ME!!
 
