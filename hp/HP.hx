b170266df52e316454412395f1b88a224879ef04
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-02-11 15:46:34 +0000

    github website not workign anymore.... fuck. maybe need to buy an actual laptop.

--- hp/HP.hx
+++ hp/HP.hx
@@ -15,7 +15,7 @@ import hxd.snd.Manager;
 // try to keep only references of stuff, no actual objects (except maybe simple types / constants)
 class HP {
 	// TODO: learn how references work in Haxe, and how to create an actual copy
-	// TODO: inline everything?
+	// TODO: inline everything?  need to learn when not to use inline; super important for graphics drawing/rendering
 	// use FP in FlashPunk and HXP in HaxePunk as models
 	
 	// TODO: singleton? or not?
@@ -26,11 +26,12 @@ class HP {
 	// but that would still clog MyApp with a ton of method functions...
 	// ... and it just makes sense to keep it seperate from App
 	
-	public static init(app:App, scene:Scene, window:Window, soundManager:Manager, ?console:Console) {
+	public static init(app:App, scene2d:Scene, scene3d:Scene, window:Window, soundManager:Manager, ?console:Console) {
 		// init ds
 		// global references to singletons..? lol. this can't be good...
 		HP.app = app;
-		HP.scene = scene; // just feels better using a proper reference rather than a getter
+		HP.scene = scene2d; // just feels better using a proper reference rather than a getter
+		HP.scene3d = scene3d; // TODO: can you have both at the same time, or just choose one?
 		HP.window = window;
 		
 		HP.engine = app.engine;
@@ -45,41 +46,43 @@ class HP {
 
 	// generally don't touch these, abstract them here
 	// should be private, but kept public for advanced use
-	public static var window:Window(default, never);
+	// currently these next few are set by reference, hence default get
+	public static inline var window:Window(default, never); // todo: lol, can you inline a property, not the function?
 	// overrides setter 'scene = new Scene(...)' to call app.setScene()
-	public static var scene:Scene(default, set);
-	public static var app:App(default, never);
+	public static inline var scene:Scene(default, set); // TODO: not might be an inline problem
+	public static inline var app:App(default, never);
 	
 	// from other places
-	public static var soundManager:Manager(default, never);
+	public static inline var soundManager:Manager(default, never);
 	
 	// PunkApp
-	public static var dt:Float(get, never); // TODO: are basic types not stored by reference?
+	// todo: if dt is indeed stored by value, that would be problem as i only update it once per frame
+	public static inline var dt:Float(get, never); // TODO: are basic types not stored by reference?
 	#if debug
-	public static var console:Console;
-	public static var screenInputHandler:Interactive; // TODO temp
+	public static inline var console:Console;
+	public static inline var screenInputHandler:Interactive; // TODO temp
 	#end
 		
 	// abstract Window
-	public static var windowTitle(default, set):String; // try to keep class name in var name for now, for learning purposes
-	public static var windowDisplayMode(default, set):DisplayMode;
-	//public static var windowWidth(get, default):Float; // unimpl; no need, yet
-	//public static var windowHeight(get, default):Float; // unimpl
+	public static inline var windowTitle(default, set):String; // try to keep class name in var name for now, for learning purposes
+	public static inline var windowDisplayMode(default, set):DisplayMode;
+	//public static inline var windowWidth(get, default):Float; // unimpl; no need, yet
+	//public static inline var windowHeight(get, default):Float; // unimpl
 
 	// abstract Scene
 	// the scene inherits a lot of shit that you probably don't want to touch..
-	public static var sceneScaleMode(get, set):ScaleMode; // use this to actually set the screen size
-	public static var sceneWidth(get, never):Float;
-	public static var sceneHeight(get, never):Float;
-	public static var width(get, never):Float; // shortcut
-	public static var height(get, never):Float;
+	public static inline var sceneScaleMode(get, set):ScaleMode; // use this to actually set the screen size
+	public static inline var sceneWidth(get, never):Float;
+	public static inline var sceneHeight(get, never):Float;
+	public static inline var width(get, never):Float; // shortcut, def inline
+	public static inline var height(get, never):Float;
 
 
 	// abstract App
-	public static var engine(get, never):Engine; // don't know what this is yet... has backgroundColor tho
-	//public static var sevents(get, never):hxd.SceneEvents; // don't know what this is yet...
-	//public static var state(get, null):MainLoopState; // unimpl
-	//public static var isPaused:Bool; // unimpl
+	public static inline var engine(get, never):Engine; // currently using a reference; don't know what this is yet... has backgroundColor tho
+	//public static inline var sevents(get, never):hxd.SceneEvents; // don't know what this is yet...
+	//public static inline var state(get, null):MainLoopState; // unimpl
+	//public static inline var isPaused:Bool; // unimpl
 
 
 
@@ -89,29 +92,28 @@ class HP {
 
 	// HP.window, HP.scene, HP.app all have "real/physical" reference vars set in PunkApp
 	static function set_scene(s:hxd.SceneEvents.InteractiveScene, disposePrevious:Bool = true) app.setScene(s, disposePrevious);
-	//static function get_scene() return app.s3d; // TODO: ?? has both at the same time? 3d unimpl
 
 	// other properties
-	//static function get_soundManager() return hxd.snd.Manager.get();
+	//static inline function get_soundManager() return hxd.snd.Manager.get();
 
 	// PunkApp properties
-	//static function get_dt() return hxd.Timer.dt;  // TODO: dt passed into update vs hxd.Timer.dt? (currently using passed in as reference)
+	//static inline function get_dt() return hxd.Timer.dt;  // TODO: dt passed into update vs hxd.Timer.dt? (currently using passed in as reference)
 	
 	// Window properties
-	//static function get_window() return hxd.Window.getInstance(); // magically generates a pointer var...??
-	static function set_windowTitle(s:String):String return HP.window.title;
-	static function set_windowDisplayMode(dm:DisplayMode):DisplayMode return HP.window.displayMode = dm;
+	//static inline function get_window() return hxd.Window.getInstance(); // currently using a reference
+	static inline function set_windowTitle(s:String):String return HP.window.title;
+	static inline function set_windowDisplayMode(dm:DisplayMode):DisplayMode return HP.window.displayMode = dm;
 	//HP.window.setFullScreen is deprecated, but can make my own..
-	//static function get_engine() return app.engine;
-	//static function get_sevents() return app.sevents;
+	//static inline function get_engine() return app.engine; // currently using a reference
+	//static inline function get_sevents() return app.sevents; // unimpl
 
 	// Scene properties
-	static function get_sceneScaleMode() return sceneScaleMode;
-	static function set_sceneScaleMode(sm:ScaleMode):ScaleMode return scene.scaleMode = sm;
-	static function get_sceneWidth() return scene.width;
-	static function get_sceneHeight() return scene.height;
-	static function get_width() return scene.width; // TODO: test window.width vs scene.width
-	static function get_height() return scene.height;
+	static inline function get_sceneScaleMode() return sceneScaleMode;
+	static inline function set_sceneScaleMode(sm:ScaleMode):ScaleMode return scene.scaleMode = sm;
+	static inline function get_sceneWidth() return scene.width;
+	static inline function get_sceneHeight() return scene.height;
+	static inline function get_width() return scene.width;
+	static inline function get_height() return scene.height;
 	
 	// App properties
 	
