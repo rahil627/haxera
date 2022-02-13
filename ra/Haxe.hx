262c464d83cc9102a2de4f97500eccb3f92543d1
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-02-13 10:22:50 +0000

    no Int32.high?

--- ra/Haxe.hx
+++ ra/Haxe.hx
@@ -1,13 +1,14 @@
-package ra; // is this not implied by the path name?
+package ra; 
 
 import hxd.Math;
+import haxe.Int64;
 
 // a helper/utility ds for the haxe programming language (including std lib)
 // more for my own personal use / not useful to others
 // note to self: there should no dependencies beyond the haxe package
 class Haxe { // todo: HaxeHelper.hx? HH import alias
 
-	public static inline function randomInt(max = Int64.high):Int { // TODO: 32bit max?
+	public static inline function randomInt(max = Int64.high):Int { // TODO: couldn't find 32-bit max, will crash on 32-bit systems!!!
 		return std.int(Math.random(max));
 	}	
 	
