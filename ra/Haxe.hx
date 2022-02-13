cc90ab830f6283c44e62ad6937a0c6ffb5f19a98
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-02-13 13:00:08 +0000

    i'm an idiot!

--- ra/Haxe.hx
+++ ra/Haxe.hx
@@ -1,16 +1,15 @@
 package ra; 
 
 import hxd.Math;
-import haxe.Int64;
 
 // a helper/utility ds for the haxe programming language (including std lib)
 // more for my own personal use / not useful to others
 // note to self: there should no dependencies beyond the haxe package
 class Haxe { // todo: HaxeHelper.hx? HH import alias
 
-	public static inline function randomInt(max:Int = Int64.high):Int { // TODO: couldn't find 32-bit max, will crash on 32-bit systems!!!
-		return std.int(Math.random(max));
-	}	
+	public static inline function randomInt(max:Int):Int { // TODO: cannot find 32-bit max
+		return Std.int(Math.random(max));
+	}
 	
 	public static inline function randomHex():Int {
 		return randomInt(0xFFFFFF);
