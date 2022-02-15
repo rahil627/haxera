76d03ff38692ae6ef7911e20a05e269dc492f204
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-02-15 22:40:12 +0000

    TOUCH INPUTS WORK, OMFG!#!@#!#

--- hp/input/Touch.hx
+++ hp/input/Touch.hx
@@ -22,19 +22,62 @@ class Touch {
 	public static var justBeganTouching(get, null):Bool; // "just" might be too verbose :/
 	public static var justStoppedTouching(get, null):Bool; // just let go'd?
 	// note: stuff like onOver/onOut are handled by Interactive, not here
+
+
+	// note: scene.mouseX/Y and window.mouseX/Y don't work for touches!
+	//public static inline function get_x() return HP.scene.mouseX; // note: scene-based event, not global
+	//public static inline function get_y() return HP.scene.mouseY;
 	
-	
-	// note: h2d.Scene provides mouseX/mouseY, but h3d.scene.Scene doesn't...
-	// hence why this only works for 2d
-	
-	public static inline function get_x() return HP.scene.mouseX; // note: scene-based event, not global
-	public static inline function get_y() return HP.scene.mouseY;
-	
-	// for 3d, can try using either Window.mouseX/Y or perhaps from hxd.SceneEvents
 	
 	public static inline function get_isTouching():Bool return Key.isDown(Key.MOUSE_LEFT);
 	public static inline function get_justBeganTouching():Bool return Key.isPressed(Key.MOUSE_LEFT); // isPressed is confusing :(
 	public static inline function get_justStoppedTouching():Bool return Key.isReleased(Key.MOUSE_LEFT);
 	
-	
+
+
+	// I FOUND IT !!!!
+	// it's scene.startCapture() / stopCapture()
+	/*
+	public function init(s:h2d.Scene) {
+		s.startCapture(
+
+	}
+	*/
+
+
+
+
+	// TODO: need to write a callback handler for EPush, EMove, ERelease events (hxd.SceneEvents?)
+
+
+
+	/*
+	// copy-pasta from Scene
+	// for scene-level implementation
+	inline function screenXToViewport(mx:Float) {
+		return @:privateAccess interactiveCamera.screenXToCamera(window.mouseX, window.mouseY);
+	}
+
+	inline function screenYToViewport(my:Float) {
+		return @:privateAccess interactiveCamera.screenYToCamera(window.mouseX, window.mouseY);
+	}
+
+	function get_mouseX() {
+		syncPos();
+		var dx = screenXToViewport(window.mouseX) - absX;
+		if( matC == 0 ) return dx / matA;
+		var dy = screenYToViewport(window.mouseY) - absY;
+		return (dx * matD - dy * matC) / (matA * matD - matB * matC);
+	}
+
+	function get_mouseY() {
+		syncPos();
+		var dy = screenYToViewport(window.mouseY) - absY;
+		if( matB == 0 ) return dy / matD;
+		var dx = screenXToViewport(window.mouseX) - absX;
+		return (dy * matA - dx * matB) / (matA * matD - matB * matC);
+	}
+	*/
+
+
 }
