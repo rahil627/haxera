6087f87cebd78e8f4d701f375d8fc80c46a321e1
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-04-16 00:41:32 +0000

    oops, forgot somethin'

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-haxe compile-and-build.hxml
+haxe build.hxml
 cp -f index.html /var/www/vhosts/rahilpatel.com/httpdocs/test.html
 cp -f app.js.map /var/www/vhosts/rahilpatel.com/httpdocs/
 cp -f app.js /var/www/vhosts/rahilpatel.com/httpdocs/
